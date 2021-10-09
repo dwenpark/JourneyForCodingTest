0c9292329858c229c12977b98b33b99739c9e167
dwenpark <dwenup@gmail.com>
2021-10-09 13:09:12 +0900

    programmers 더 맵게

---
+++ heap/programmers 더 맵게.py
@@ -0,0 +1,13 @@
+# https://programmers.co.kr/learn/courses/30/lessons/42626
+
+import heapq
+
+def solution(scoville, K):
+    answer = 0
+    heapq.heapify(scoville)
+    while (scoville[0] < K):
+        if len(scoville) == 1:
+            return -1
+        heapq.heappush(scoville, heapq.heappop(scoville) + (heapq.heappop(scoville) * 2))
+        answer += 1
+    return answer
