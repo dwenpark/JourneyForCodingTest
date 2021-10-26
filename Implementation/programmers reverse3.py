08dd72673d0bc5e2ffaef7a955d39f68318f5cf8
dwenpark <dwenup@gmail.com>
2021-10-26 21:58:02 +0900

    프로그래머스 3진법 뒤집기

---
+++ Implementation/programmers reverse3.py
@@ -0,0 +1,8 @@
+# https://programmers.co.kr/learn/courses/30/lessons/68935
+
+def solution(n):
+    rev_base = ''
+    while n > 0:
+        n, mod = divmod(n, 3)
+        rev_base += str(mod)
+    return int(rev_base, 3)
