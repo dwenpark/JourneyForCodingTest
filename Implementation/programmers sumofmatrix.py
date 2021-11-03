a8797d66aaf152901122406f78394da1142d0eba
dwenpark <dwenup@gmail.com>
2021-11-04 00:00:09 +0900

    프로그래머스 행렬의 덧셈

---
+++ Implementation/programmers sumofmatrix.py
@@ -0,0 +1,10 @@
+# https://programmers.co.kr/learn/courses/30/lessons/12950
+
+def solution(arr1, arr2):
+    answer = []
+    for i in range(len(arr1)):
+        tmp = []
+        for j in range(len(arr1[0])):
+            tmp.append(arr1[i][j]+arr2[i][j])
+        answer.append(tmp)
+    return answer
