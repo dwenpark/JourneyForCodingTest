ecea7b07bd2ce3f34a87fbc2a9bf7ae2b5adf9e2
dwenpark <dwenup@gmail.com>
2023-01-26 00:02:42 +0900

    Add: 20230125

--- backtracking, bruteforce/baekjoon 2580.py
+++ backtracking, bruteforce/baekjoon 2580.py
@@ -12,23 +12,31 @@ for i in range(9):
 		if value == 0:
 			zero.append([i,j])
 		div[(i // 3) * 3 + (j // 3)].append(value)
-
+		
 while zero:
 	for i in zero:
 		a , b = i[0], i[1]
 		if sdk[a].count(0) == 1:
 			sdk[a][b] = 45 - sum(sdk[a])
-			div[(a // 3) * 3 + (b // 3)][(a // 3) * 3 + (b % 3)] = sdk[a][b]
+			div[(b // 3) * 3 + (a // 3)][(a % 3) * 3 + (b % 3)] = sdk[a][b]
+			print(i, sdk[a][b], 1)
+			zero.remove(i)
 		elif [row[b] for row in sdk].count(0) == 1:
 			sdk[a][b] = 45 - sum([row[b] for row in sdk])
-			div[(a // 3) * 3 + (b // 3)][(a // 3) * 3 + (b % 3)] = sdk[a][b]
+			div[(b // 3) * 3 + (a // 3)][(a % 3) * 3 + (b % 3)] = sdk[a][b]
+			print(i, sdk[a][b], 2)
+			zero.remove(i)
 		else:
 			for k in div:
 				if k.count(0) == 1:
-					div[(a // 3) * 3 + (b // 3)][(a // 3) * 3 + (b % 3)] = 45 - sum(k)
-					sdk[a][b] = div[(a // 3) * 3 + (b // 3)][(a // 3) * 3 + (b % 3)]
+					div[(b // 3) * 3 + (a // 3)][(a % 3) * 3 + (b % 3)] = 45 - sum(k)
+					sdk[a][b] = div[(b // 3) * 3 + (a // 3)][(a % 3) * 3 + (b % 3)]
+					print(i, sdk[a][b], 3)
+					zero.remove(i)
 					break
-		zero.remove(i)
+
+		
+		
 
 for k in sdk:
 	print(*k)
